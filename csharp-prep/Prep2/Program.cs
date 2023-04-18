7488e80efe272cff5c558c1934e2f98dff03fafe
febarnett3 <113536068+febarnett3@users.noreply.github.com>
2023-04-17 18:54:43 -0600

    Week 1 Prep 2 Completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,62 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your score percentage? ");
+        string userScore = Console.ReadLine();
+        int score = int.Parse(userScore);
+        
+        //finds the letter score
+        
+        string letterScore = "E";
+        int secondDigit = score%10;
+        
+        if (score >= 90)
+        {
+            letterScore = "A";
+        }
+         else if (score >= 80)
+        {
+            letterScore = "B";
+        }
+        else if (score >= 70)
+        {
+            letterScore = "C";
+        }
+        else if (score >= 60)
+        {
+            letterScore = "D";
+        }
+        else if (score < 60)
+        {
+            letterScore = "F";
+        }
+
+        //finds +/-
+        if (letterScore != "F")
+        {
+            if (secondDigit >= 7)
+            {
+                letterScore = letterScore + "+";
+            }
+            else if (secondDigit < 3)
+            {
+                letterScore = letterScore + "-";
+            }
+        }
+
+        //prints the letter score
+        Console.WriteLine(letterScore);
+
+        //determines whether or not the user passed.
+        if (score >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed.");
+        }
+
     }
 }
